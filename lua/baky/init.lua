3674bbaaa92dd60203b2e6dba9a2365dddb9024d
Baktash Gorgani <baktash.gorgani@alticeusa.com>
2024-01-07 20:39:53 -0500

    line numbers

--- lua/baky/init.lua
+++ lua/baky/init.lua
@@ -1,4 +1,6 @@
 require("baky.remap")
+vim.wo.relativenumber = true
+vim.wo.number = true
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
