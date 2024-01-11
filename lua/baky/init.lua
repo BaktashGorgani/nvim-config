58e1ba0ced8eacc6e0093d11206e3db8a0fc0624
Baktash Gorgani <baktash.gorgani@alticeusa.com>
2024-01-10 19:27:29 -0500

    white space markers

--- lua/baky/init.lua
+++ lua/baky/init.lua
@@ -1,6 +1,8 @@
 require("baky.remap")
 vim.wo.relativenumber = true
 vim.wo.number = true
+vim.opt.list = true
+vim.opt.listchars = { eol = '⏎' , trail = '●' , nbsp = '⎵' , extends = '…' , precedes = '…' , space = '·' , tab = '  ┊' }
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
