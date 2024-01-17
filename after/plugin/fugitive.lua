82653670faa1d10733887d693b3898d2941ba393
Baktash Gorgani <baktash.gorgani@gmail.com>
2024-01-17 11:34:21 -0500

    Better keymaps

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -2,3 +2,6 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
 vim.keymap.set("n", "<leader>gf", "<cmd>Git pull<cr>")
 vim.keymap.set("n", "<leader>gp", "<cmd>Git push<cr>")
+
+--Git Log
+vim.keymap.set("n", "<leader>gl", "<cmd>Git log<cr>")
