38c669c8e8caa0caf7725ebe42f0e40715f1d94a
Baktash Gorgani <baktash.gorgani@gmail.com>
2025-05-11 16:37:23 -0400

    refactor(telescope,autocmd): simplify keymaps and update yank highlight
    - Use direct builtin mappings for Telescope keymaps, replacing custom
      functions with builtin methods for find_files and live_grep.
    - Update yank highlight autocmd to use vim.hl.on_yank with on_macro=true,
      replacing deprecated vim.highlight.on_yank.

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,9 +1,7 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', "<cmd>lua require'telescope.builtin'.find_files({no_ignore=true})<cr>", {})
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
+vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
 
 --Extension keymaps
 vim.keymap.set("n", "<leader>u", "<cmd>Telescope undo<cr>")
