2d671510c438c699f16aef053acc2b2a1df87836
Baktash Gorgani <17418270+BaktashGorgani@users.noreply.github.com>
2025-07-13 06:59:30 -0400

    dont want my file tree to follow what file i'm on

--- after/plugin/neotree.lua
+++ after/plugin/neotree.lua
@@ -34,15 +34,11 @@ require("neo-tree").setup({
             hide_hidden = false, -- only works on Windows for hidden files/directories
         },
         follow_current_file = {
-            enabled = true, -- This will find and focus the file in the active buffer every time
-            --               -- the current file is changed while the tree is open.
             leave_dirs_open = true, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
         },
     },
     buffers = {
         follow_current_file = {
-            enabled = true, -- This will find and focus the file in the active buffer every time
-            --              -- the current file is changed while the tree is open.
             leave_dirs_open = true, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
         },
     }
